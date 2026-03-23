d3ffeb9d422f6ff8d8c7ba02c8d92daf066b6841
Edwin Daniels <stratease@gmail.com>
2026-03-23 16:14:57 +0200

    Fix php compat definition

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -144,6 +144,7 @@ class AdminController {
 		$is_debug = defined( 'WP_DEBUG' ) && WP_DEBUG && defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG;
 		?>
 		<div class="wrap">
+			<span class="wp-header-end"></span>
 			<div id="flux-media-optimizer-app">
 			<?php if ( $is_debug ) : ?>
 				<div class="notice notice-warning" style="margin: 20px 0; padding: 15px;">
